5985e0e9ce2fbee88853eb73ca1b4bf1c2c4107c
lzl <d>
2019-07-03 20:28:26 +0800

    first commit

---
+++ src/main/java/cn/lzl/shixi/ShixiApplication.java
@@ -0,0 +1,13 @@
+package cn.lzl.shixi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShixiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShixiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/lzl/shixi/ShixiApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.lzl.shixi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShixiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
